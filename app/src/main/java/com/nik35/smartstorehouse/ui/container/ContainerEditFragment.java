527f83c6e10f00b27b8daac137cc9b23b6e0a1b1
YesNik <wr35nik@gmail.com>
2020-09-26 21:04:42 +0300

    add test recycler in fragments

--- app/src/main/java/com/nik35/smartstorehouse/ui/container/ContainerEditFragment.java
+++ app/src/main/java/com/nik35/smartstorehouse/ui/container/ContainerEditFragment.java
@@ -3,10 +3,17 @@ package com.nik35.smartstorehouse.ui.container;
 import android.os.Bundle;
 
 import com.nik35.smartstorehouse.R;
+import com.nik35.smartstorehouse.adapters.RecyclerAdapter;
+import com.nik35.smartstorehouse.adapters.RecyclerModel;
 import com.nik35.smartstorehouse.ui.BaseFragment;
+import com.nik35.smartstorehouse.utils.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.Nullable;
-import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ContainerEditFragment extends BaseFragment {
 
@@ -20,6 +27,20 @@ public class ContainerEditFragment extends BaseFragment {
 
     @Override
     protected void initView(@Nullable Bundle bundle) {
-        $(R.id.nav_to_home).setOnClickListener(view -> Navigation.findNavController(view).navigate(R.id.homeFragment));
+
+        List<RecyclerModel> recyclerModels = new ArrayList<>();
+
+        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_INSIDE_ITEM));
+        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_INSIDE_ITEM));
+        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_INSIDE_ITEM));
+        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_INSIDE_ITEM));
+        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_INSIDE_ITEM));
+
+        RecyclerView editRecycler = $(R.id.edit_recycler);
+
+        RecyclerAdapter adapter = new RecyclerAdapter(this, recyclerModels);
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        editRecycler.setLayoutManager(manager);
+        editRecycler.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/nik35/smartstorehouse/ui/home/HomeFragment.java
+++ app/src/main/java/com/nik35/smartstorehouse/ui/home/HomeFragment.java
@@ -2,10 +2,17 @@ package com.nik35.smartstorehouse.ui.home;
 
 import android.os.Bundle;
 import com.nik35.smartstorehouse.R;
+import com.nik35.smartstorehouse.adapters.RecyclerAdapter;
+import com.nik35.smartstorehouse.adapters.RecyclerModel;
 import com.nik35.smartstorehouse.ui.BaseFragment;
+import com.nik35.smartstorehouse.utils.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.Nullable;
-import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeFragment extends BaseFragment {
 
@@ -19,6 +26,20 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     protected void initView(@Nullable Bundle bundle) {
-        $(R.id.nav_to_edit).setOnClickListener(view -> Navigation.findNavController(view).navigate(R.id.containerEditFragment));
+
+        List<RecyclerModel> recyclerModels = new ArrayList<>();
+
+        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_ITEM));
+        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_ITEM));
+        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_ITEM));
+        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_ITEM));
+        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_ITEM));
+
+        RecyclerView homeRecycler = $(R.id.home_recycler);
+
+        RecyclerAdapter adapter = new RecyclerAdapter(this, recyclerModels);
+        GridLayoutManager manager = new GridLayoutManager(getContext(), 2);
+        homeRecycler.setLayoutManager(manager);
+        homeRecycler.setAdapter(adapter);
     }
 }
