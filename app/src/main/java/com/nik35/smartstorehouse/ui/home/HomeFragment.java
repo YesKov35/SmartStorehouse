a93762d50a17d8abdc8851e0ddc233d9ca44ffe7
YesNik <wr35nik@gmail.com>
2020-09-27 20:40:56 +0300

    add FloatingActionButton with hide logic

--- app/src/main/java/com/nik35/smartstorehouse/ui/home/HomeFragment.java
+++ app/src/main/java/com/nik35/smartstorehouse/ui/home/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.nik35.smartstorehouse.ui.home;
 
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.nik35.smartstorehouse.R;
 import com.nik35.smartstorehouse.adapters.RecyclerAdapter;
 import com.nik35.smartstorehouse.adapters.RecyclerModel;
@@ -10,6 +12,7 @@ import com.nik35.smartstorehouse.utils.Constants;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,10 +33,6 @@ public class HomeFragment extends BaseFragment {
         List<RecyclerModel> recyclerModels = new ArrayList<>();
 
         recyclerModels.add(new RecyclerModel(Constants.CONTAINER_ITEM));
-        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_ITEM));
-        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_ITEM));
-        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_ITEM));
-        recyclerModels.add(new RecyclerModel(Constants.CONTAINER_ITEM));
 
         RecyclerView homeRecycler = $(R.id.home_recycler);
 
@@ -41,5 +40,28 @@ public class HomeFragment extends BaseFragment {
         GridLayoutManager manager = new GridLayoutManager(getContext(), 2);
         homeRecycler.setLayoutManager(manager);
         homeRecycler.setAdapter(adapter);
+
+        FloatingActionButton addContainer = $(R.id.add_container);
+
+        addContainer.setOnClickListener(view -> {
+            recyclerModels.add(new RecyclerModel(Constants.CONTAINER_ITEM));
+            adapter.notifyDataSetChanged();
+        });
+
+        homeRecycler.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                if (dy > 0)
+                    addContainer.hide();
+                else
+                    addContainer.show();
+            }
+
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+        });
     }
 }
